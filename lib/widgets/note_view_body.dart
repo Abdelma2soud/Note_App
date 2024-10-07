ae361779216d6f1c5fe83ac87e4557fcc83096e7
Abdelma2soud <abdelmaqsoud83@gmail.com>
2024-10-07 21:10:45 +0300

    create custome appbar

---
+++ lib/widgets/note_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [CustomAppBar()],
+        ),
+      ),
+    );
+  }
+}
